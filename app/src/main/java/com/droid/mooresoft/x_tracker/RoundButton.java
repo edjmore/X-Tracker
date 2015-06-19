abf9900a5948693bf7cf4b9cefd28f18d37db6a4
Ed Moore <edmoore7@gmail.com>
2015-06-19 14:13:02 -0400

    Messed around with button.

--- app/src/main/java/com/droid/mooresoft/x_tracker/RoundButton.java
+++ app/src/main/java/com/droid/mooresoft/x_tracker/RoundButton.java
@@ -7,12 +7,14 @@ import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Interpolator;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.RadialGradient;
 import android.graphics.Shader;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.Button;
 
